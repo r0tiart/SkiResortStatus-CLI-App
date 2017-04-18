1e641bf4db3ee7af674b38ce85d1fc0dd96f3810
antley chu <antley.chu@gmail.com>
2017-04-18 20:28:40 +0000

    updated menu/submenus in cli

--- lib/Ski_Resort_Status/cli.rb
+++ lib/Ski_Resort_Status/cli.rb
@@ -17,25 +17,100 @@ class SkiResortStatus::CLI
       puts "2. All"
 
       input = gets.chomp
+      @region_number = input
 
       case input
       when "1"
-        specific_region(input)
+        region_menu(input)
       when "2"
-        specific_region(input)
+        region_menu(input)
       when "exit"
         puts "Logging out"
       else
         puts "Please re-enter the region number"
         input = gets.chomp
+        @region_number = input
       end
-    # end
   end
 
-  def specific_region(region_number)
+  def region_menu(region_number)
     region = region_number.to_i - 1
-    region
+
+    puts "--------#{region}--------"
+    puts ""
+    puts "What would you like to see for the #{region} region"
+    puts "  1) List all open resort as of (update date)"
+    puts "  2) List all resorts only open on weekends"
+    puts "  3) List all resorts that are open or open on weekends only"
+    puts "  4) List all resorts including closed resorts"
+    puts "  5) List all closed resorts"
+
+    input = gets.chomp
+    case input
+    when "1"
+      open_resorts
+    when "2"
+      weekend_resorts
+    when "3"
+      all_open
+    when "4"
+      all_resorts
+    when "5"
+      closed_resorts
+    when "main"
+      regions
+    when "exit"
+      puts "Logging out"
+    else
+      "Please re-enter input"
+      input = gets.chomp
+    end
   end
 
+  def open_resorts
+    puts "all open resorts"
+    restart?
+  end
+
+  def weekend_resorts
+    puts "weekend only resorts"
+    restart?
+  end
+
+  def all_open
+    puts "here are all the open resorts inclusive of weekend only resorts"
+    restart?
+  end
+
+  def all_resorts
+    puts "all resorts"
+    restart?
+  end
+
+  def closed_resorts
+    puts "closed resorts"
+    restart?
+  end
+
+  def restart?
+    puts ""
+    puts "Would you like to go back to your specify region menu?"
+    puts""
+    puts "If not please type exit or main to continue"
+
+    input = gets.chomp
+
+    case input
+    when "yes"
+      region_menu(@region_number)
+    when "exit"
+      puts "Logging out"
+    when "main"
+      regions
+    else
+      puts "Please type exit or main to continue"
+      input = gets.chomp
+    end
+  end
 
 end
