faccd5f4157ab4b18e0e15aa6622e1a9bcaba5ed
antley chu <antley.chu@gmail.com>
2017-04-18 20:04:37 +0000

    started CLI interface - with regions puts menu

--- lib/Ski_Resort_Status/cli.rb
+++ lib/Ski_Resort_Status/cli.rb
@@ -1,6 +1,41 @@
 class SkiResortStatus::CLI
   def call
-    puts "Please select Country (US/CAD)"
-    input = gets.chomp 
+    puts "***********************************"
+    puts "*    Welcome to the Snow Status    *"
+    puts "***********************************"
+    puts ""
+
+    regions
   end
+
+  def regions
+
+    # while input != "exit"
+      puts "Please Choose Region - by number"
+      puts ""
+      puts "1. Regions"
+      puts "2. All"
+
+      input = gets.chomp
+
+      case input
+      when "1"
+        specific_region(input)
+      when "2"
+        specific_region(input)
+      when "exit"
+        puts "Logging out"
+      else
+        puts "Please re-enter the region number"
+        input = gets.chomp
+      end
+    # end
+  end
+
+  def specific_region(region_number)
+    region = region_number.to_i - 1
+    region
+  end
+
+
 end
