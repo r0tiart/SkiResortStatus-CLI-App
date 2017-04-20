c88812afa7613c96e1df56c1f9948ffeb78dd451
antley chu <antley.chu@gmail.com>
2017-04-20 20:28:33 +0000

    located, resort name and depths

--- lib/Ski_Resort_Status/scraper.rb
+++ lib/Ski_Resort_Status/scraper.rb
@@ -10,10 +10,19 @@ class SkiResortStatus::Scraper
 
   def scrape
     doc = Nokogiri::HTML(open(self.url))
-    rows = doc.css("tr")
-    rows.pop
-    rows.shift
-    rows.shift
-    rows
+    @rows = doc.css("tr")
+    @rows.pop
+    @rows.shift
+    @rows.shift
+    @rows
+  end
+  binding.pry
+
+  def create_from_scrape
+    @rows.each.do |table_row|
+    name = table_row.css(".name").text
+    depth = table_row.css(".rMid.c").css("b").text.split(" - ")
+    base_depth = depth.first.gsub("\"", " inches")
+    upper_depth = depth.last.gsub("\"", " inches")
   end
 end
